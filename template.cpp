74844f42bb8478c8545f0d03af9e7b3e3679cca0
Jatin Salve <66058203+jatinS-dev@users.noreply.github.com>
2021-06-08 23:20:12 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,71 @@
+#pragma optimization_level 3
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize("Ofast")//Comment optimisations for interactive problems (use endl)
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")        
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+ 
+template<typename T1,typename T2>istream& operator>>(istream& in,pair<T1,T2> &a){in>>a.fr>>a.sc;return in;}
+template<typename T1,typename T2>ostream& operator<<(ostream& out,pair<T1,T2> a){out<<a.fr<<" "<<a.sc;return out;}
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+ 
+const long long INF=1e18;
+const int32_t M=1e9+7;
+const int32_t MM=998244353;
+const int MOD = 1e9 + 7;
+const long long MOD2 = static_cast<long long>(MOD) * MOD;
+
+void solve(){
+    // Write your code here
+} 
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #ifdef SIEVE
+        sieve();
+    #endif
+    #ifdef NCR
+        init();
+    #endif
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    return 0;
+}
