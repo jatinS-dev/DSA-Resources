050c79a1761a143d8344221856a81a69bce09b32
Jatin Salve <66058203+jatinS-dev@users.noreply.github.com>
2021-12-07 16:01:53 +0530

    Create Activity_selection.cpp

---
+++ Greedy/Activity_selection.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+    public:
+    
+    static bool comp(pair<int, int> a, pair<int, int> b) {
+        if(a.second == b.second) {
+            return (a.first < b.first);
+        } else {
+            return (a.second < b.second);
+        }
+    }
+    
+    int maxMeetings(int start[], int end[], int n) {
+        vector<pair<int, int>> v(n);
+        for(int i = 0; i < n; i++) {
+            v[i] = {start[i], end[i]};
+        }
+        sort(v.begin(), v.end(), comp);
+       int i = 0, j = 1, ans = 0;
+        while(j < n) {
+            if(v[i].second < v[j].first) {
+                ans++;
+                i = j;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return ans+1;
+    }
+};
