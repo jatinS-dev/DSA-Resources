e89b01115a385f34ef142a1797320c5c92d390a9
Jatin Salve <66058203+jatinS-dev@users.noreply.github.com>
2022-12-31 18:02:24 +0530

    Update frog_jump.cpp

--- Dynamic Programming/frog_jump.cpp
+++ Dynamic Programming/frog_jump.cpp
@@ -18,3 +18,21 @@ int frogJump(int n, vector<int> &heights) {
     vector<int> dp(n+1, -1);
     return f(n-1, heights, dp);
 }
+
+
+///// TOP_DOWN
+
+#include <bits/stdc++.h> 
+int frogJump(int n, vector<int> &heights) {
+    vector<int> dp(n, -1);
+    dp[0] = 0;
+    for(int i = 1; i < n; i++) {
+        int left = dp[i-1] + abs(heights[i] - heights[i-1]);
+        int right = INT_MAX;
+        if(i > 1) {
+         right = dp[i-2] + abs(heights[i] - heights[i-2]);
+        }
+        dp[i] = min(left, right);
+    }
+    return dp[n-1];
+}
